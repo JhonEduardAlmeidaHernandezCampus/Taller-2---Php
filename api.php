cd4a7da2482fbc1680c386f01b40151d25f58402
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-05-23 21:58:00 -0500

    feat(Ejercicios): :rocket: Ejercicio 2
    Desarrollo ejercicio 2 con diferentes tipos de validaciones de inputs
    
    by: Jhon Hernandez

--- api.php
+++ api.php
@@ -2,41 +2,36 @@
 
 header("Content-Type:application/json");
 
-$_DATA = json_decode(file_get_contents("php://input"), true); // Recoge todos los datos de los input
-$_METHOD = $_SERVER["REQUEST_METHOD"];                        // Valida si el metodo es POST 
+$_DATA = json_decode(file_get_contents("php://input"), true); 
+$_METHOD = $_SERVER["REQUEST_METHOD"];                        
 
-function promedio(float $nota1, float $nota2, float $nota3){
-    $promedio = ($nota1 + $nota2 + $nota3)/3;
-
-    return (array)[
-        "prom"=> $promedio,
-        "mensaje"=> ($promedio >= 3.9) ?"Becado" :"Estudie"
-    ];
+function validarNumero(float $numero){
+    if (intval($numero) == floatval($numero)) {
+        $calculo = $numero % 2;
+        $mensajeParImpar = ($calculo == 0) ? "PAR." : "IMPAR.";
+        $mensajeMayorDiez = ($numero > 10) ? "El número $numero es mayor a 10, y es " : "";
+        
+        return $mensajeMayorDiez . "" . $mensajeParImpar;
+    } else {
+        return "Error!! El dato ingresado es incorrecto";
+    }     
 }
 
 // --------------------------------------------------------------------------
 try {
     $res = match($_METHOD){
-        "POST" => promedio(...$_DATA),
+        "POST" => validarNumero(...$_DATA),
         default => <<<STRING
         Methodo "${_METHOD}" No se puede ejecutar esta operacion
         STRING,
-    };                                                                       // Valida si el metodo es POST
+    };                                                                       
 } catch (\Throwable $th) {
-    $res = (array)[
-        "prom"=> "Error!",
-        "mensaje"=> "Error!"
-    ];
+    $res = "Error!";
 }
 // --------------------------------------------------------------------------
 
 // -----------------------------------------------
-$resuesta = (array) [
-    "Estado" =>$res["mensaje"],
-    "Datos" => $_DATA,                              // Se muestra la respuesta en un array en pantalla 
-    "Promedio" => $res["prom"]
-];
-echo json_encode($resuesta, JSON_PRETTY_PRINT);
+echo $res;
 // -----------------------------------------------
 
 ?>
--- main.js
+++ main.js
@@ -3,16 +3,18 @@ let myForm = document.querySelector("#myForm");
 myForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const guardarDatos = Object.fromEntries(new FormData(e.target));
-    let config = {
-        method: "POST",
-        header: {"Content-Type":"application/json"},
-        body: JSON.stringify(guardarDatos)
+
+    if(guardarDatos.numero == ""){
+        alert("Por favor llenar los campos");
+    } else {
+        let config = {
+            method: "POST",
+            header: {"Content-Type":"application/json"},
+            body: JSON.stringify(guardarDatos)
+        }
+        let res = await( await fetch("api.php", config)).text();
+        document.querySelector("pre").innerHTML = res;   
     }
-    let res = await( await fetch("api.php", config)).text();
-    document.querySelector("pre").innerHTML = res;
 })
 
-// Construir el algoritmo para un programa que ingrese:
-// tres notas de un alumno 
-// si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“
-// de lo contrario un mensaje que diga "becado"
+// Dado un número indicar si es par o impar y si es mayor de 10
